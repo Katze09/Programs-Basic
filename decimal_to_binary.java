a61a1effc13a1c1afd893eaf135b044903757f61
Katze09 <57643201+Katze09@users.noreply.github.com>
2020-11-09 17:44:59 -0600

    Add files via upload

---
+++ decimal_to_binary.java
@@ -0,0 +1,17 @@
+public String decimal_A_binario(int num) {
+    String binary = "";
+    if(num > 0){
+        int[] binarioList = new int[50];
+        int counter = 0;
+        while (num > 1) {
+            binarioList[counter] = num % 2;
+            num = num / 2;
+            counter++;
+        }
+        binarioList[counter] = 1;
+        for (int i = counter; i >= 0; i--) 
+            binary = binary + binarioList[i];
+    }else
+        binary = "0";
+    return binary;
+}
